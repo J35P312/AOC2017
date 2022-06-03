3a56af01ba8f656ee0d853b99f936e24e3835ccd
J35P312 <jesper.eisfeldt@scilifelab.se>
2022-06-03 23:37:45 +0200

    modified:   D3_1.py

--- jesper/D3/D3_1.py
+++ jesper/D3/D3_1.py
@@ -4,8 +4,8 @@ import math
 
 
 #the input
-#target=312051
-target=16
+target=312051
+#target=1024
 
 #we start with a box of one layer, and add layers until the box contain our target.
 #Each time we add a layer the number of elements in the box will grow with n*4+4, n is the length of the box of current layer.
@@ -26,6 +26,11 @@ while current_max[-1] < target:
 	#add two for the new "corners"
 	row_length.append(row_length[-1]+2)
 
+
+#the position of the 1, i.e the middle of the matrix
+one=math.ceil(row_length[-1]/2)
+print(one)
+
 #Now we know the layer of our target, next we will find its position within a row in the outer layer
 
 #make lists containing all numbers in the rows of the outer layers:
@@ -34,44 +39,43 @@ upper_row=list(range(current_max[-2]+row_length[-1]-1,current_max[-2]+2*row_leng
 left_row=list(range(current_max[-2]+row_length[-1]+row_length[-1]-2,current_max[-2]+3*row_length[-1]-2))
 lower_row=list(range(current_max[-2]+3*row_length[-1]-3,current_max[-2]+4*row_length[-1]-3))
 
-pos=0
 #check which list contains our target:
-if target < current_max[-2]+row_length[-1]:
+if target < max(right_row):
+	#the target is on the right side
 	print("right")
+	x=row_length[-1]
+
 	for i in range(0,len(right_row)):
 		if right_row[i] == target:
+			y=i+2
 
-			if i == math.ceil(len(right_row)/2):
-				pos=0
-			elif i <= math.ceil(len(right_row)/2):
-				pos=math.ceil(len(right_row)/2)-i-1
-			else:
-				pos=i-math.ceil(len(right_row)/2)
-
-
-
-elif target < current_max[-2]+2*row_length[-1]-1:
-	print("upper")
+elif target < max(upper_row):
+	#target is on the upper side
+	print("up")
+	y=row_length[-1]
+	
 	for i in range(0,len(upper_row)):
 		if upper_row[i] == target:
+			x=i+1
 
-			if i == math.ceil(len(upper_row)/2):
-				pos=0
-				print("hej")
-			elif i <= math.ceil(len(upper_row)/2):
-				pos=math.ceil(len(upper_row)/2)-i
-			else:
-				pos=1+i-math.ceil(len(upper_row)/2)
-
-	
-elif target < current_max[-2]+3*row_length[-1]-2:
+elif target < max(left_row):
+	#target is on the left side
 	print("left")
+	x=1
+	
+	for i in range(0,len(left_row)):
+		if left_row[i] == target:
+			y=i+1
+
 else:
-	print("lower")
+	#target is on the bottom
+	y=1	
+	for i in range(0,len(lower_row)):
+		if lower_row[i] == target:
+			x=i+1
+
 
 #number of layers
 print(math.ceil(len(current_max)/2))
-#element within outer layer
-print(pos)
-#distance
-print(pos+math.ceil(len(current_max)/2))
+#manhattan distance
+print( abs(x-one)+abs(y-one) )
