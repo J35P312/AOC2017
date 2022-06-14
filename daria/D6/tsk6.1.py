efefc3f7baa994dc0b69f4936e34f68a6e9ac8da
Daria Nikolaenko <vvitalevnad@gmail.com>
2022-06-14 18:17:27 +0300

    adding 6.1

---
+++ daria/D6/tsk6.1.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import copy
+
+
+df = pd.read_csv('input.txt', delimiter='\t', header=None)
+df_list = df.loc[0,:].values.tolist()
+memory = [int(x) for x in df_list]
+
+ 
+def main():
+
+    loops = 0
+    memories = ([copy.copy(memory)])
+    print('MEMORIES: ', memories)
+
+    while True:
+        max_val = max(memory)
+        max_val_index = memory.index(max_val)
+        memory[max_val_index] = 0
+        index = max_val_index + 1
+
+
+        while max_val > 0:
+            
+            if index < len(memory):
+                memory[index] += 1
+                max_val -= 1
+                index += 1
+                
+            elif index >= len(memory):
+                index = 0
+                memory[index] += 1
+                index += 1
+                max_val -= 1
+
+
+        loops+=1
+
+
+        if memory[:] not in memories:
+            memories.append(memory[:])
+
+        else:
+            print('LOOPS =', loops)
+            break
+main()
